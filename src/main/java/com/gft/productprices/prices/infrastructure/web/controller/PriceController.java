acedd4e418e9a500e1b71940602288b9446b0d5f
judacon <judacon.h@gmail.com>
2023-11-17 01:49:42 +0100

    Adicion de documentacion con swagger

--- src/main/java/com/gft/productprices/prices/infrastructure/web/controller/PriceController.java
+++ src/main/java/com/gft/productprices/prices/infrastructure/web/controller/PriceController.java
@@ -4,6 +4,12 @@ import com.gft.productprices.prices.domain.ports.PriceService;
 import com.gft.productprices.prices.infrastructure.web.exceptions.PriceNotFoundException;
 import com.gft.productprices.prices.infrastructure.web.mapper.PriceResponseMapper;
 import com.gft.productprices.prices.infrastructure.web.model.PriceResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
@@ -27,11 +33,24 @@ public class PriceController {
     }
 
 
-    @GetMapping(value="/api/getPrice", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value="/api/prices", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Obtiene el precio de un producto", description = "Proporciona el precio de un producto para una marca y fecha específicas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Precio encontrado",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = PriceResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "Precio no encontrado",
+                    content = {@Content(mediaType = "application/json")})
+    })
     public ResponseEntity<PriceResponse> getPrice(
-            @RequestParam(name = "applicationDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd-HH.mm.ss" ) Date applicationDate,
+            @Parameter(description = "Fecha de aplicación para el precio, formato yyyy-MM-dd HH:mm:ss. Se utiliza la fecha y hora actual si no se proporciona.", example = "2024-12-31 12:00:00")
+            @RequestParam(name = "applicationDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss" ) Date applicationDate,
+
+            @Parameter(description = "ID del producto para el cual se busca el precio", example = "3456")
             @RequestParam(name = "productId" ) Long productId,
+
+            @Parameter(description = "ID de la marca del producto", example = "1")
             @RequestParam(name = "brandId") Integer brandId) throws PriceNotFoundException {
+
 
         if (applicationDate == null) {
             applicationDate = new Date();
--- src/main/java/com/gft/productprices/prices/infrastructure/web/model/PriceResponse.java
+++ src/main/java/com/gft/productprices/prices/infrastructure/web/model/PriceResponse.java
@@ -1,5 +1,6 @@
 package com.gft.productprices.prices.infrastructure.web.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Builder
@@ -7,12 +8,24 @@ import lombok.*;
 @AllArgsConstructor
 public class PriceResponse {
 
+    @Schema(description = "ID del producto", example = "1")
     private long productId;
-    private int brandId;
-    private int priceList;
-    private String currency;
-    private String startDate;
-    private String endDate;
-    private Double finalPrice;
 
+    @Schema(description = "ID de la marca", example = "10")
+    private int brandId;
+
+    @Schema(description = "Identificador de la lista de precios", example = "2")
+    private int priceList;
+
+    @Schema(description = "Moneda en la que se expresa el precio", example = "EUR")
+    private String currency;
+
+    @Schema(description = "Fecha de inicio de validez del precio, en formato yyyy-MM-dd HH:mm:ss", example = "2023-01-01 00:00:00")
+    private String startDate;
+
+    @Schema(description = "Fecha de fin de validez del precio, en formato yyyy-MM-dd HH:mm:ss", example = "2023-12-31 12:00:00")
+    private String endDate;
+
+    @Schema(description = "Precio final del producto", example = "99.99")
+    private Double finalPrice;
 }
